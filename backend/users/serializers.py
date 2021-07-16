1f05af2c5f2a5f653ee71872766b09b2682d8a29
Jonathan Kao <jkao97@yahoo.com>
2021-07-15 20:53:26 -0700

    feat: users serializer hashes password

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -5,4 +5,17 @@ from .models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('pk', 'email', 'first_name', 'last_name', 'date_joined')
+        fields = ('pk', 'email', 'first_name',
+                  'last_name', 'date_joined', 'last_login', 'is_active', 'is_staff', 'is_superuser', 'password')
+        read_only_fields = ('pk', 'date_joined', 'last_login',
+                            'is_active', 'is_staff', 'is_superuser')
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
+
+    def create(self, validated_data):  # ensure password is hashed
+        password = validated_data.get('password', '')
+        user = super().create(validated_data)
+        user.set_password(password)
+        user.save()
+        return user
