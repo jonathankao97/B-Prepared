785c0cff41f399e7660ff9646acfff2b426c5a26
Jonathan Kao <jkao97@yahoo.com>
2021-07-21 15:32:21 -0700

    feat: initial authentication_repository implementation

---
+++ frontend/packages/authentication_repository/lib/src/authentication_repository.dart
@@ -0,0 +1,41 @@
+import 'package:backend_client/backend_client.dart';
+
+enum UserAuthenticationStatus {
+  unknown,
+  signedIn,
+  signedOut,
+}
+
+class AuthenticationRepository {
+  AuthenticationRepository(BackendClient backendClient)
+      : _backendClient = backendClient;
+
+  final BackendClient _backendClient;
+
+  Stream<UserAuthenticationStatus> get authenticationStatus {
+    return _backendClient.authenticationStatus.map((status) {
+      switch (status) {
+        case AuthenticationStatus.unauthenticated:
+          return UserAuthenticationStatus.signedOut;
+        case AuthenticationStatus.authenticated:
+          return UserAuthenticationStatus.signedIn;
+        case AuthenticationStatus.initial:
+          return UserAuthenticationStatus.unknown;
+      }
+    });
+  }
+
+  Future<void> signIn({
+    required String username,
+    required String password,
+  }) async {
+    await _backendClient.authenticate(
+      username: username,
+      password: password,
+    );
+  }
+
+  Future<void> signOut() async {
+    await _backendClient.unauthenticate();
+  }
+}
