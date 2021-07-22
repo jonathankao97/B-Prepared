0edcee97ea2b2e1301f41445e6671bf025e05d81
Jonathan Kao <jkao97@yahoo.com>
2021-07-21 22:16:04 -0700

    feat: add simplejwt user auth

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -128,4 +128,11 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.User'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
 NUM_TESTS = 7
