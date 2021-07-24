62bfa1c7065ab5a290dbab162ad504e5e7a53176
Jonathan Kao <jkao97@yahoo.com>
2021-07-23 20:03:21 -0700

    feat: add me endpoint to retrieve current user (#36)

--- backend/users/viewsets.py
+++ backend/users/viewsets.py
@@ -1,8 +1,16 @@
 from rest_framework import viewsets
 from .models import User
 from .serializers import UserSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class UserView(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    @action(detail=False, methods=['get'])
+    def me(self, request):
+        instance = request.user
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
