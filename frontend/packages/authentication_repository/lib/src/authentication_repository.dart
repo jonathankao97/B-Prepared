b25608b2e8d415e184ba283822c896a0342bba5b
Jonathan Kao <jkao97@yahoo.com>
2021-07-26 21:35:04 -0700

    feat: add User to UserAuthenticationStatus

--- frontend/packages/authentication_repository/lib/src/authentication_repository.dart
+++ frontend/packages/authentication_repository/lib/src/authentication_repository.dart
@@ -1,9 +1,27 @@
 import 'package:backend_client/backend_client.dart';
+import 'package:user_repository/user_repository.dart';
 
-enum UserAuthenticationStatus {
+enum UserAuthenticationStatusX {
   unknown,
   signedIn,
   signedOut,
+}
+
+class UserAuthenticationStatus {
+  final UserAuthenticationStatusX status;
+  final User user;
+
+  const UserAuthenticationStatus._({required this.status, required this.user});
+
+  const UserAuthenticationStatus.unknown()
+      : this._(status: UserAuthenticationStatusX.unknown, user: User.anonymous);
+
+  const UserAuthenticationStatus.signedIn(User user)
+      : this._(status: UserAuthenticationStatusX.signedIn, user: user);
+
+  const UserAuthenticationStatus.signedOut()
+      : this._(
+            status: UserAuthenticationStatusX.signedOut, user: User.anonymous);
 }
 
 class AuthenticationRepository {
@@ -13,16 +31,22 @@ class AuthenticationRepository {
   final BackendClient _backendClient;
 
   Stream<UserAuthenticationStatus> get authenticationStatus {
-    return _backendClient.authenticationStatus.map((status) {
+    return _backendClient.authenticationStatus.asyncMap((status) async {
       switch (status) {
         case AuthenticationStatus.unauthenticated:
-          return UserAuthenticationStatus.signedOut;
+          return UserAuthenticationStatus.signedOut();
         case AuthenticationStatus.authenticated:
-          return UserAuthenticationStatus.signedIn;
+          return UserAuthenticationStatus.signedIn(await getCurrentUser());
         case AuthenticationStatus.initial:
-          return UserAuthenticationStatus.unknown;
+          return UserAuthenticationStatus.unknown();
       }
     });
+  }
+
+  Future<User> getCurrentUser() async {
+    final userResponse = await _backendClient.getCurrentUser();
+    final user = User.fromUserResponse(userResponse);
+    return user;
   }
 
   Future<void> signIn({
